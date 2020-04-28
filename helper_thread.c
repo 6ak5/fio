6ak5/fio

commit 31eca641ad91634e5ffcf369cd756b0506a700c1

Fix a potential deadlock in helper_do_stat()
pthread_mutex_lock(), pthread_cond_signal() and pthread_mutex_unlock() are
not async-signal-safe and hence must not be used inside a singal handler
implementation. Rework the code for communication with the helper thread
such that it becomes async-signal-safe.

Fixes: a47591e4923f ("Improve logging accuracy")
Signed-off-by: Bart Van Assche <bvanassche@acm.org>

helper_thread.c
@@ -10,48 +10,52 @@
 #include "steadystate.h"
 #include "pshared.h"
 
+enum action {
+	A_EXIT		= 1,
+	A_RESET		= 2,
+	A_DO_STAT	= 3,
+};
+
 static struct helper_data {
 	volatile int exit;
-	volatile int reset;
-	volatile int do_stat;
+	int pipe[2]; /* 0: read end; 1: write end. */
 	struct sk_out *sk_out;
 	pthread_t thread;
-	pthread_mutex_t lock;
-	pthread_cond_t cond;
 	struct fio_sem *startup_sem;
 } *helper_data;
 
 void helper_thread_destroy(void)
 {
-	pthread_cond_destroy(&helper_data->cond);
-	pthread_mutex_destroy(&helper_data->lock);
+	close(helper_data->pipe[0]);
+	close(helper_data->pipe[1]);
 	sfree(helper_data);
+}
+
+static void submit_action(enum action a)
+{
+	const uint8_t data = a;
+	int ret;
+
+	if (!helper_data)
+		return;
+
+	ret = write(helper_data->pipe[1], &data, sizeof(data));
+	assert(ret == 1);
 }
 
 void helper_reset(void)
 {
-	if (!helper_data)
-		return;
-
-	pthread_mutex_lock(&helper_data->lock);
-
-	if (!helper_data->reset) {
-		helper_data->reset = 1;
-		pthread_cond_signal(&helper_data->cond);
-	}
-
-	pthread_mutex_unlock(&helper_data->lock);
+	submit_action(A_RESET);
 }
 
+/*
+ * May be invoked in signal handler context and hence must only call functions
+ * that are async-signal-safe. See also
+ * https://pubs.opengroup.org/onlinepubs/9699919799/functions/V2_chap02.html#tag_15_04_03.
+ */
 void helper_do_stat(void)
 {
-	if (!helper_data)
-		return;
-
-	pthread_mutex_lock(&helper_data->lock);
-	helper_data->do_stat = 1;
-	pthread_cond_signal(&helper_data->cond);
-	pthread_mutex_unlock(&helper_data->lock);
+	submit_action(A_DO_STAT);
 }
 
 bool helper_should_exit(void)
@@ -64,14 +68,9 @@ bool helper_should_exit(void)
 
 void helper_thread_exit(void)
 {
-	void *ret;
-
-	pthread_mutex_lock(&helper_data->lock);
 	helper_data->exit = 1;
-	pthread_cond_signal(&helper_data->cond);
-	pthread_mutex_unlock(&helper_data->lock);
-
-	pthread_join(helper_data->thread, &ret);
+	submit_action(A_EXIT);
+	pthread_join(helper_data->thread, NULL);
 }
 
 static void *helper_thread_main(void *data)
@@ -79,6 +78,7 @@ static void *helper_thread_main(void *data)
 	struct helper_data *hd = data;
 	unsigned int msec_to_next_event, next_log, next_ss = STEADYSTATE_MSEC;
 	struct timespec ts, last_du, last_ss;
+	uint8_t action;
 	int ret = 0;
 
 	sk_out_assign(hd->sk_out);
@@ -96,11 +96,23 @@ static void *helper_thread_main(void *data)
 	msec_to_next_event = DISK_UTIL_MSEC;
 	while (!ret && !hd->exit) {
 		uint64_t since_du, since_ss = 0;
+		struct timeval timeout = {
+			.tv_sec  = DISK_UTIL_MSEC / 1000,
+			.tv_usec = (DISK_UTIL_MSEC % 1000) * 1000,
+		};
+		fd_set rfds, efds;
 
 		timespec_add_msec(&ts, msec_to_next_event);
 
-		pthread_mutex_lock(&hd->lock);
-		pthread_cond_timedwait(&hd->cond, &hd->lock, &ts);
+		if (read(hd->pipe[0], &action, sizeof(action)) < 0) {
+			FD_ZERO(&rfds);
+			FD_SET(hd->pipe[0], &rfds);
+			FD_ZERO(&efds);
+			FD_SET(hd->pipe[0], &efds);
+			select(1, &rfds, NULL, &efds, &timeout);
+			if (read(hd->pipe[0], &action, sizeof(action)) < 0)
+				action = 0;
+		}
 
 #ifdef CONFIG_PTHREAD_CONDATTR_SETCLOCK
 		clock_gettime(CLOCK_MONOTONIC, &ts);
@@ -108,13 +120,10 @@ static void *helper_thread_main(void *data)
 		clock_gettime(CLOCK_REALTIME, &ts);
 #endif
 
-		if (hd->reset) {
-			memcpy(&last_du, &ts, sizeof(ts));
-			memcpy(&last_ss, &ts, sizeof(ts));
-			hd->reset = 0;
+		if (action == A_RESET) {
+			last_du = ts;
+			last_ss = ts;
 		}
-
-		pthread_mutex_unlock(&hd->lock);
 
 		since_du = mtime_since(&last_du, &ts);
 		if (since_du >= DISK_UTIL_MSEC || DISK_UTIL_MSEC - since_du < 10) {
@@ -126,10 +135,8 @@ static void *helper_thread_main(void *data)
 		} else
 			msec_to_next_event = DISK_UTIL_MSEC - since_du;
 
-		if (hd->do_stat) {
-			hd->do_stat = 0;
+		if (action == A_DO_STAT)
 			__show_running_run_stats();
-		}
 
 		next_log = calc_log_samples();
 		if (!next_log)
@@ -173,9 +180,16 @@ int helper_thread_create(struct fio_sem *startup_sem, struct sk_out *sk_out)
 
 	hd->sk_out = sk_out;
 
-	ret = mutex_cond_init_pshared(&hd->lock, &hd->cond);
+#ifdef __linux__
+	ret = pipe2(hd->pipe, O_CLOEXEC);
+#else
+	ret = pipe(hd->pipe);
+#endif
 	if (ret)
 		return 1;
+
+	ret = fcntl(hd->pipe[0], F_SETFL, O_NONBLOCK);
+	assert(ret >= 0);
 
 	hd->startup_sem = startup_sem;
 

commit 92f824865ad93a809de76c8b2eb74e749b687c30

helper_thread: fix inconsistent status intervals
The signal handler safety changes to the helper thread have resulted in
inconsistent status-interval intervals. Consider the following:

$ ./fio-canonical/fio --name=test --rw=randwrite --ioengine=libaio --direct=1 --runtime=180 --time_based --filename=/dev/fioa --output=write-canonical.out --minimal --status-interval=1
...
$ cut -d ';' -f 50 < write-canonical.out | awk 'NR>1{print $1-p} {p=$1}' | sort -n | tail
1002
1002
1002
1002
1002
1042
1046
1251
1252
1252

Several of the status-interval output lines are ~1250ms apart.

This patch moves code for triggering the status-interval output from the main
fio process to the helper thread. The resulting intervals are much closer to
the desired 1000ms.

$ ./fio/fio --name=test --rw=randwrite --ioengine=libaio --direct=1 --runtime=180 --time_based --filename=/dev/fioa --minimal --status-interval=1 --output=write-test.out
...
$ cut -d ';' -f 50 < write-test.out | awk 'NR>1{print $1-p} {p=$1}' | sort -n | tail
1001
1001
1001
1001
1001
1001
1001
1001
1001
1001

Reported-by: <nate.rivers@wdc.com>
Fixes: 31eca641ad91 ("Fix a potential deadlock in helper_do_stat()")

helper_thread.c
@@ -131,8 +131,9 @@ void helper_thread_exit(void)
 static void *helper_thread_main(void *data)
 {
 	struct helper_data *hd = data;
-	unsigned int msec_to_next_event, next_log, next_ss = STEADYSTATE_MSEC;
-	struct timespec ts, last_du, last_ss;
+	unsigned int msec_to_next_event, next_log, next_si;
+	unsigned int next_ss = STEADYSTATE_MSEC;
+	struct timespec ts, last_du, last_ss, last_si;
 	char action;
 	int ret = 0;
 
@@ -157,12 +158,13 @@ static void *helper_thread_main(void *data)
 #endif
 	memcpy(&last_du, &ts, sizeof(ts));
 	memcpy(&last_ss, &ts, sizeof(ts));
+	memcpy(&last_si, &ts, sizeof(ts));
 
 	fio_sem_up(hd->startup_sem);
 
 	msec_to_next_event = DISK_UTIL_MSEC;
 	while (!ret && !hd->exit) {
-		uint64_t since_du, since_ss = 0;
+		uint64_t since_du, since_si, since_ss = 0;
 		struct timeval timeout = {
 			.tv_sec  = msec_to_next_event / 1000,
 			.tv_usec = (msec_to_next_event % 1000) * 1000,
@@ -206,6 +208,20 @@ static void *helper_thread_main(void *data)
 
 		if (action == A_DO_STAT)
 			__show_running_run_stats();
+
+		if (status_interval) {
+			since_si = mtime_since(&last_si, &ts);
+			if (since_si >= status_interval || status_interval - since_si < 10) {
+				__show_running_run_stats();
+				timespec_add_msec(&last_si, since_si);
+				if (since_si > status_interval)
+					next_si = status_interval - (since_si - status_interval);
+				else
+					next_si = status_interval;
+			} else
+				next_si = status_interval - since_si;
+			msec_to_next_event = min(next_si, msec_to_next_event);
+		}
 
 		next_log = calc_log_samples();
 		if (!next_log)

stat.c
@@ -2354,8 +2354,6 @@ void __show_running_run_stats(void)
 	fio_sem_up(stat_sem);
 }
 
-static bool status_interval_init;
-static struct timespec status_time;
 static bool status_file_disabled;
 
 #define FIO_STATUS_FILE		"fio-dump-status"
@@ -2398,16 +2396,6 @@ static int check_status_file(void)
 
 void check_for_running_stats(void)
 {
-	if (status_interval) {
-		if (!status_interval_init) {
-			fio_gettime(&status_time, NULL);
-			status_interval_init = true;
-		} else if (mtime_since_now(&status_time) >= status_interval) {
-			show_running_run_stats();
-			fio_gettime(&status_time, NULL);
-			return;
-		}
-	}
 	if (check_status_file()) {
 		show_running_run_stats();
 		return;